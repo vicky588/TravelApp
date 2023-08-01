5f7b9c165e3cc75e32ad2c3cf487b81c42a5be42
vicky588 <vickyray588@gmail.com>
2023-08-01 12:31:05 +0530

    resolved conflict with remote main

---
+++ App.tsx
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () =>{
+return (
+ <View style={{backgroundColor:'red',width:'90%',height:'90%',margin:10}}>
+
+ </View>
+
+)
+}
+export default App;
